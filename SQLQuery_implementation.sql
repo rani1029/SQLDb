a9e3722d765fdb69100779c8f4054ddc27f9711d
rani1029 <ranig1029@gmail.com>
2021-12-20 07:49:25 +0530

    order by,group by,having query implemented

--- SQLQuery_implementation.sql
+++ SQLQuery_implementation.sql
@@ -3,3 +3,25 @@ SELECT DISTINCT University_type FROM UniversityDetails;
 
 SELECT * FROM student_Details
 WHERE department IN ('engg','arts');
+
+---order by
+
+select * from UniversityDetails order by estd;
+
+select * from UniversityDetails order by No_of_colleges desc;
+
+====================================================================================================================================
+--group by 
+--query for every university type how many id is there
+
+SELECT COUNT(id), University_type
+FROM UniversityDetails
+GROUP BY University_type;
+
+================================================================================================================================================
+---having
+ 
+SELECT COUNT(id), University_type
+FROM UniversityDetails
+GROUP BY University_type
+having COUNT(id)=3;
